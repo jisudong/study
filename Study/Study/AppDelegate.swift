99a288f4aa8392be9296f9486210ff1f2ea2aa52
jisudong <136225101@qq.com>
2017-06-23 22:42:01 +0800

    测试引用第三方库

--- Study/Study/AppDelegate.swift
+++ Study/Study/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RxCocoa
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
---
+++ Study/Study/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  Study
+//
+//  Created by 纪素东 on 2017/6/23.
+//  Copyright © 2017年 syswin. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+@import RxSwift
+@import RxCocoa
+
+#endif /* Header_h */
