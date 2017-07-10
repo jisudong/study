e6375899ed639d7d98cc2fd366298b24489339ef
jisudong <jisudong@syswin.com>
2017-07-10 18:23:26 +0800

    Filtering and Conditional Operators

--- RxSwift-master/Rx.playground/Pages/Playground.xcplaygroundpage/Contents.swift
+++ RxSwift-master/Rx.playground/Pages/Playground.xcplaygroundpage/Contents.swift
@@ -385,4 +385,164 @@ example("flatMap and flatMapLatest") {
 }
 
 
+// MARK: Filtering and Conditional Operators
+
+example("filter") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of(
+        "🐱", "🐰", "🐶",
+        "🐸", "🐱", "🐰",
+        "🐹", "🐸", "🐱")
+        .filter { $0 == "🐱" }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("distinctUntilChanged") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("🐱", "🐷", "🐱", "🐱", "🐱", "🐵", "🐱")
+        .distinctUntilChanged()
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("elementAt") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .elementAt(3)
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("single") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .single()
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("single with conditions") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .single { $0 == "🐸" }
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .single { $0 == "🐰" }
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .single { $0 == "🔵" }
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+}
+
+example("take") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .take(3)
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+}
+
+example("takeLast") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .takeLast(3)
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+}
+
+example("takeWhile") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .takeWhile { $0 < 4 }
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+}
+
+example("takeUntil") {
+    let disposeBag = DisposeBag()
+    
+    let sourceSequence = PublishSubject<String>()
+    let referenceSequence = PublishSubject<String>()
+    
+    sourceSequence
+        .takeUntil(referenceSequence)
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+    
+    sourceSequence.onNext("🐱")
+    sourceSequence.onNext("🐰")
+    sourceSequence.onNext("🐶")
+    
+    referenceSequence.onNext("🔴")
+    
+    sourceSequence.onNext("🐸")
+    sourceSequence.onNext("🐷")
+    sourceSequence.onNext("🐵")
+}
+
+example("skip") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .skip(2)
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("skipWhile") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .skipWhile { $0 < 4 }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("skipWhileWithIndex") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("🐱", "🐰", "🐶", "🐸", "🐷", "🐵")
+        .skipWhileWithIndex({ (element, index) in
+            index < 3
+        })
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("skipUntil") {
+    let disposeBag = DisposeBag()
+    
+    let sourceSequence = PublishSubject<String>()
+    let referenceSequence = PublishSubject<String>()
+    
+    sourceSequence
+        .skipUntil(referenceSequence)
+        .subscribe(onNext: { print($0)})
+        .disposed(by: disposeBag)
+    
+    sourceSequence.onNext("🐱")
+    sourceSequence.onNext("🐱")
+    sourceSequence.onNext("🐶")
+    
+    referenceSequence.onNext("🔴")
+    
+    sourceSequence.onNext("🐸")
+    sourceSequence.onNext("🐷")
+    sourceSequence.onNext("🐵")
+}
+
 
