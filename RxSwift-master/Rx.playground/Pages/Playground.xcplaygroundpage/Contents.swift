9f6a9f749dfdbaf110ada84fd724a9370b2c7f0a
jisudong <jisudong@syswin.com>
2017-07-10 11:53:23 +0800

    combining operators

--- RxSwift-master/Rx.playground/Pages/Playground.xcplaygroundpage/Contents.swift
+++ RxSwift-master/Rx.playground/Pages/Playground.xcplaygroundpage/Contents.swift
@@ -223,12 +223,119 @@ example("Variable") {
 example("startWith") {
     let disposeBag = DisposeBag()
     
-    Observable.of("", "", "", "")
-        .startWith("")
-        .startWith("")
-        .startWith("", "", "")
+    Observable.of("🐶", "🐱", "🐭", "🐹")
+        .startWith("1️⃣")
+        .startWith("2️⃣")
+        .startWith("3️⃣", "🅰️", "🅱️")
         .subscribe(onNext: { print($0) })
         .disposed(by: disposeBag)
+}
+
+example("merge") {
+    let disposeBag = DisposeBag()
+    
+    let subject1 = PublishSubject<String>()
+    let subject2 = PublishSubject<String>()
+    
+    Observable.of(subject1, subject2)
+        .merge()
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+    
+    subject1.onNext("🅰️")
+    
+    subject1.onNext("🅱️")
+    
+    subject2.onNext("①")
+    
+    subject2.onNext("②")
+    
+    subject1.onNext("🆎")
+    
+    subject2.onNext("③")
+}
+
+example("zip") {
+    let disposeBag = DisposeBag()
+    
+    let stringSubject = PublishSubject<String>()
+    let intSubject = PublishSubject<Int>()
+    
+    Observable.zip(stringSubject, intSubject) { stringElement, intElement in
+            "\(stringElement) \(intElement)"
+        }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+    
+    stringSubject.onNext("🅰️")
+    stringSubject.onNext("🅱️")
+    
+    intSubject.onNext(1)
+    intSubject.onNext(2)
+
+    stringSubject.onNext("🆎")
+    intSubject.onNext(3)
+}
+
+example("combineLatest") {
+    let disposeBag = DisposeBag()
+    
+    let stringSubject = PublishSubject<String>()
+    let intSubject = PublishSubject<Int>()
+    
+    Observable.combineLatest(stringSubject, intSubject) { stringElement, intElement in
+            "\(stringElement) \(intElement)"
+        }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+    
+    stringSubject.onNext("🅰️")
+    
+    stringSubject.onNext("🅱️")
+    intSubject.onNext(1)
+
+    intSubject.onNext(2)
+    
+    stringSubject.onNext("🆎")
+}
+
+example("Array.combineLatest") {
+    let disposeBag = DisposeBag()
+    
+    let stringObservable = Observable.just("🔴")
+    let fruitObservable = Observable.from(["🍎", "🍐", "🍊"])
+    let animalObservable = Observable.of("🐶", "🐱", "🐭", "🐹")
+    
+    
+    
+    Observable.combineLatest([stringObservable, fruitObservable, animalObservable]) {
+            "\($0[0]) \($0[1]) \($0[2])"
+        }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("switchLatest") {
+    let disposeBag = DisposeBag()
+    
+    let subject1 = BehaviorSubject(value: "⚽️")
+    let subject2 = BehaviorSubject(value: "🍎")
+    
+    let variable = Variable(subject1)
+    
+    variable.asObservable()
+        .switchLatest()
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+    
+    subject1.onNext("🏈")
+    subject1.onNext("🏀")
+    
+    variable.value = subject2
+    
+    subject1.onNext("⚾️")
+    
+    subject2.onNext("🍐")
 }
 
 
