64e507006898973cb201841cae936118c57b0625
jisudong <jisudong@syswin.com>
2017-07-19 15:11:28 +0800

    error handling operators

--- RxSwift-master/Rx.playground/Pages/Playground.xcplaygroundpage/Contents.swift
+++ RxSwift-master/Rx.playground/Pages/Playground.xcplaygroundpage/Contents.swift
@@ -695,4 +695,105 @@ func sampleWithMulticast() {
 //sampleWithMulticast()
 
 
+// MARK: Error Handling Operators
+
+example("catchErrorJustReturn") {
+    let disposeBag = DisposeBag()
+    
+    let sequenceThatFails = PublishSubject<String>()
+    
+    sequenceThatFails
+        .catchErrorJustReturn("😊")
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+    
+    sequenceThatFails.onNext("😬")
+    sequenceThatFails.onNext("😨")
+    sequenceThatFails.onNext("😡")
+    sequenceThatFails.onNext("🔴")
+    sequenceThatFails.onError(TestError.test)
+}
+
+example("catchError") {
+    let disposeBag = DisposeBag()
+    
+    let sequenceThatFails = PublishSubject<String>()
+    let recoverySequence = PublishSubject<String>()
+    
+    sequenceThatFails
+        .catchError {
+            print("Error:", $0)
+            return recoverySequence
+        }
+        .subscribe { print($0) }
+        .disposed(by: disposeBag)
+    
+    sequenceThatFails.onNext("😬")
+    sequenceThatFails.onNext("😨")
+    sequenceThatFails.onNext("😡")
+    sequenceThatFails.onNext("🔴")
+    sequenceThatFails.onError(TestError.test)
+    
+    recoverySequence.onNext("😊")
+}
+
+example("retry") {
+    let disposeBag = DisposeBag()
+    
+    var count = 1
+    
+    let sequenceThatErrors = Observable<String>.create({ observer in
+        observer.onNext("🍎");
+        observer.onNext("🍐");
+        observer.onNext("🍊")
+        
+        if count == 1 {
+            observer.onError(TestError.test)
+            print("Error encountered")
+            count += 1
+        }
+        
+        observer.onNext("🐶")
+        observer.onNext("🐱")
+        observer.onNext("🐭")
+        observer.onCompleted()
+        
+        return Disposables.create()
+    })
+
+    sequenceThatErrors
+        .retry()
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example("retry maxAttemptCount") {
+    let disposeBag = DisposeBag()
+    
+    var count = 1
+    
+    let sequenceThatErrors = Observable<String>.create { observer in
+        observer.onNext("🍎")
+        observer.onNext("🍐");
+        observer.onNext("🍊")
+        
+        if count < 5 {
+            observer.onError(TestError.test)
+            print("Error encountered")
+            count += 1
+        }
+        
+        observer.onNext("🐶")
+        observer.onNext("🐱")
+        observer.onNext("🐭")
+        observer.onCompleted()
+        
+        return Disposables.create()
+    }
+    
+    sequenceThatErrors
+        .retry(3)
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
 
